7201f919e29ea4decfc8b72988f8564b70c81de1
Brian DiRito <bdirito@gmail.com>
2018-11-14 14:55:06 -0500

    added functionality to navbar (home & dropdown)

--- mod_3_frontend/src/index.js
+++ mod_3_frontend/src/index.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     const container = document.getElementById('container')
     const tourButton = document.getElementById("tour")
+    const navbar = document.querySelector('nav')
     const navDropdown = document.getElementById('nav-dropdown')
 
     const metUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
@@ -15,18 +16,31 @@ window.addEventListener('DOMContentLoaded', function(){
     getPaintings()
     getDepartments()
 
-    // console.log(paintingsArray)
-    // console.log(departmentsArray)
-
-    tourButton.addEventListener("click", function(e){
-        name = e.target.innerText
-        tourLocation = departmentsArray.find(object => {
-            return object.name === name
-        })
-        tour(tourLocation, paintingIndex)
+    navbar.addEventListener("click", function(e){
+        if (e.target.parentElement.id === "nav-dropdown"){
+            name = e.target.innerText
+            tourLocation = departmentsArray.find(object => {
+                return object.name === name
+            })
+            tour(tourLocation, paintingIndex)
+        } else if (e.target.innerText === "Home") {
+            container.innerHTML = `<h1 class="the-met">The MET</h1>
+            <div class="w3-dropdown-hover" id="tour">
+             <button class="button is-light the-met-start">Start Tour</button>
+             <div class="w3-dropdown-content w3-bar-block w3-card-4 dropdown">
+               <a class="w3-bar-item w3-button">European Paintings</a>
+               <a class="w3-bar-item w3-button">Medieval Art</a>
+               <a class="w3-bar-item w3-button">Modern and Contemporary Art</a>
+               <a class="w3-bar-item w3-button">The American Wing</a>
+               <a class="w3-bar-item w3-button">Asian Art</a>
+               <a class="w3-bar-item w3-button">Greek and Roman Art</a>
+             </div>
+           </div>
+       </div>`
+        }
     })
 
-    navDropdown.addEventListener("click", function(e){
+    tourButton.addEventListener("click", function(e){
         name = e.target.innerText
         tourLocation = departmentsArray.find(object => {
             return object.name === name
